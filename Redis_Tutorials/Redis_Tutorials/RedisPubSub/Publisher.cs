60a16984c7c3eb3c87587f957ef8349c096bd7be
Hau Nguyen <quanghau05@gmail.com>
2021-06-14 14:56:49 +0700

    Implement pub/sub sample

---
+++ Redis_Tutorials/Redis_Tutorials/RedisPubSub/Publisher.cs
@@ -0,0 +1,32 @@
+﻿using Redis_Tutorials.Helper;
+using System;
+
+namespace Redis_Tutorials.RedisPubSub
+{
+    public class Publisher
+    {
+        public static void PublishMessageToSingleChannel()
+        {
+            const string channelName = "Channel";
+
+            var publisher = RedisConnectionHelper.Connection.GetSubscriber();
+
+            // Publish to a channel
+            publisher.Publish(channelName, $"Publish a message to channel: {channelName}");
+
+            Console.WriteLine("Publish - Done");
+        }
+
+        public static void PublishMessageToChannelPattern()
+        {
+            const string channelPattern = "Channel.*";
+
+            var publisher = RedisConnectionHelper.Connection.GetSubscriber();
+
+            // publish message to channels which match the pattern
+            publisher.Publish(channelPattern, $"Publish a message to literal channel: channelPattern");
+
+            Console.WriteLine("Publish - Done");
+        }
+    }
+}
---
+++ Redis_Tutorials/Redis_Tutorials/RedisPubSub/Subcriber.cs
@@ -0,0 +1,34 @@
+﻿using Redis_Tutorials.Helper;
+using StackExchange.Redis;
+using System;
+
+namespace Redis_Tutorials.RedisPubSub
+{
+    public class Subcriber
+    {
+        public static void SubscribeMessageFromChannel()
+        {
+            const string channel = "Channel";
+
+            var subscriber = RedisConnectionHelper.Connection.GetSubscriber();
+            Console.WriteLine("Start subcribe message...");
+            subscriber.Subscribe(channel, (channel, message) =>
+            {
+                Console.WriteLine($"{DateTime.Now.ToString("yyyyMMdd HH:mm:ss")}<Normal - {channel}><{message}>.");
+            });
+        }
+
+        public static void SubscribeMessageWithChannelPattern()
+        {
+            const string channelPattern = "Channel.*";
+
+            var subscriber = RedisConnectionHelper.Connection.GetSubscriber();
+            var subPatternChannel = new RedisChannel(channelPattern, RedisChannel.PatternMode.Pattern);
+            Console.WriteLine("Start subcribe message...");
+            subscriber.Subscribe(subPatternChannel, (channel, message) =>
+            {
+                Console.WriteLine($"{DateTime.Now.ToString("yyyyMMdd HH:mm:ss")}<Pattern - {channel}><{message}>.");
+            });
+        }
+    }
+}
