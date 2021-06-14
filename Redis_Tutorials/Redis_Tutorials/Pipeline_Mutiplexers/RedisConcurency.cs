188b2da067a54a4698b675afa676eff39c172351
Hau Nguyen <quanghau05@gmail.com>
2021-06-14 14:57:11 +0700

    Implement pipe - mutiplexers sample

---
+++ Redis_Tutorials/Redis_Tutorials/Pipeline_Mutiplexers/RedisConcurency.cs
@@ -0,0 +1,41 @@
+﻿using Redis_Tutorials.Helper;
+using StackExchange.Redis;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Redis_Tutorials.Pipeline_Mutiplexers
+{
+    public class RedisConcurency
+    {
+        public static async Task ReadDataAsync()
+        {
+            var database = RedisConnectionHelper.GetRedisDatabase();
+            var devicesCount = 100;
+            Console.WriteLine("Reading data from cache");
+
+            var tasks = Enumerable.Range(0, devicesCount).Select(c => GetStringAsync(database, c));
+            await Task.WhenAll(tasks);
+
+            Console.WriteLine("Reading data from cache - END");
+        }
+
+        private static async Task GetStringAsync(IDatabase database, int count)
+        {
+            var value = await database.StringGetAsync($"Device_Status:{count}");
+            Console.WriteLine($"Valor={value}");
+        }
+
+        public static async Task SaveDataAsync()
+        {
+            var devicesCount = 100;
+            var database = RedisConnectionHelper.GetRedisDatabase();
+            Console.WriteLine("Saving random data in cache");
+
+            var tasks = Enumerable.Range(0, devicesCount).Select(c => database.StringSetAsync($"Device_Status:{c}", c));
+            await Task.WhenAll(tasks);
+
+            Console.WriteLine("Saving random data in cache - END");
+        }
+    }
+}
--- Redis_Tutorials/Redis_Tutorials/Program.cs
+++ Redis_Tutorials/Redis_Tutorials/Program.cs
@@ -2,16 +2,27 @@
 {
     using Redis_Tutorials.BasicCache;
     using Redis_Tutorials.Helper;
+    using Redis_Tutorials.Pipeline_Mutiplexers;
+    using Redis_Tutorials.RedisPubSub;
     using StackExchange.Redis;
     using System;
+    using System.Threading.Tasks;
 
     class Program
     {
         static void Main(string[] args)
         {
             // Basic usage
-            BasicCaching.SaveData();
-            BasicCaching.ReadData();
+            //BasicCaching.SaveData();
+            //BasicCaching.ReadData();
+
+            // Pipeline & Multiplexers
+            //await RedisConcurency.SaveDataAsync();
+            //await RedisConcurency.ReadDataAsync();
+
+            // Pub/Sub
+            Publisher.PublishMessageToChannelPattern();
+            Subcriber.SubscribeMessageWithChannelPattern();
 
             Console.ReadKey();
         }
